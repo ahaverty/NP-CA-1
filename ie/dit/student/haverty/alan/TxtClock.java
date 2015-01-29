f06ba20a9868fac02d461355ac82ea86ab8efa89
Alan Haverty <alan@ahaverty.com>
2015-01-29 23:08:26 +0000

    Initial TxtClock program commit.
    Initial commit of the TxtClock program for CA1 Network Programming DIT.
    Program is using RegEx to validate 24hr time strings, also extracting
    the data from the strings into Calendar objects and reprinting as 24hr
    times to console with True or False depending on validatin function.
    Some test cases implemented, Program is also taking in one parameter
    which is validated and reprinted after extraction. TODO Need to document
    code better and extract test cases into their own class, need to include
    NTP and other functions and the time to english text function.

---
+++ ie/dit/student/haverty/alan/TxtClock.java
@@ -0,0 +1,141 @@
+package ie.dit.student.haverty.alan;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TxtClock {
+
+	// RegEx string for 24hr time with option of including seconds.
+	// Seconds nested grouped for easier extraction without extracting colon.
+	private static String time_pattern = "([01]?[0-9]|2[0-3]):([0-5][0-9])(:([0-5][0-9]))?";
+	private static Pattern pattern;
+
+	/**
+	 * TODO Main function
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		pattern = Pattern.compile(time_pattern);
+		
+		// TODO extract tests into their own class
+		testCaseTimes();
+		
+		// TODO encapsulate the following better
+		if (args.length == 1) {
+			
+			String userInput = args[0];
+			Calendar time;
+
+			if (validateTime(userInput)) {
+
+				time = extractTime(userInput);
+
+				System.out.print("True\t");
+
+				System.out.print(timeToDigitString(time));
+			} else {
+				System.out.print("False\t");
+				System.out.print(userInput);
+			}
+		} else {
+			System.out.println("Incorrect amount of arguments provided.");
+			System.out.println("Please provide a time in 24hr format seperated by colons.");
+			System.out.println("Seconds are optional. HH:MM:SS or HH:MM");
+		}
+
+	}
+
+	/**
+	 * Validates an input string using a RegEx pattern matcher.
+	 * 
+	 * @param time
+	 *            in string format HH:MM:SS or HH:MM
+	 * @return True if the input string is a valid time, False if it is not
+	 */
+	private static boolean validateTime(String time) {
+		Matcher matcher = pattern.matcher(time);
+
+		return matcher.matches();
+	}
+
+	/**
+	 * Extracts the time into a calendar object taken from the input string
+	 * using a RegEx pattern matcher.
+	 * 
+	 * @param time
+	 *            in string format HH:MM:SS or HH:MM
+	 * @return A blank Calendar object with the Hour(24hr), Minute and Second(00
+	 *         if not in string)
+	 */
+	private static Calendar extractTime(String time) {
+		Matcher matcher = pattern.matcher(time);
+
+		Calendar calendar = Calendar.getInstance();
+		// clearing calendar for cases without seconds defined
+		calendar.clear();
+
+		if (matcher.find()) {
+			calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(matcher.group(1)));
+			calendar.set(Calendar.MINUTE, Integer.parseInt(matcher.group(2)));
+
+			if (matcher.group(4) != null) {
+				calendar.set(Calendar.SECOND, Integer.parseInt(matcher.group(4)));
+			}
+		}
+
+		return calendar;
+	}
+
+	/**
+	 * Converts a Calendar object to a string taking the Hour, Minutes and
+	 * Seconds.
+	 * 
+	 * @param cal
+	 * @return string in the form "HH:MM:SS"
+	 */
+	private static String timeToDigitString(Calendar cal) {
+		String timeAsString = new String();
+
+		// using string formatter to add '0' in front of single digit times.
+		// e.g 1:45 -> 01:45
+		timeAsString += String.format("%02d", cal.get(Calendar.HOUR_OF_DAY));
+		timeAsString += ":";
+		timeAsString += String.format("%02d", cal.get(Calendar.MINUTE));
+		timeAsString += ":";
+		timeAsString += String.format("%02d", cal.get(Calendar.SECOND));
+
+		return timeAsString;
+	}
+
+	/**
+	 * Function to hold temporary test cases
+	 */
+	private static void testCaseTimes() {
+
+		String[] testTimes = { "23:40:54", "5:29", "01:00:03", "23:59:59",
+				"14:10:", "26:40:54", "01:61:54", "20:40:72", "", "alan",
+				"-1:40:09" };
+		Calendar time;
+
+		for (String timeText : testTimes) {
+
+			if (validateTime(timeText)) {
+
+				time = extractTime(timeText);
+
+				System.out.print("True\t");
+
+				System.out.print(timeToDigitString(time));
+			} else {
+				System.out.print("False\t");
+				System.out.print(timeText);
+			}
+
+			System.out.print("\n");
+
+		}
+	}
+
+}
